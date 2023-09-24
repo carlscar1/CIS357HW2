1fb9a04aff9fd51c723edc5d7b0a23f615599e78
Alexandra MacKay <mackayal@mail.gvsu.edu>
2023-09-24 17:58:51 -0400

    Added labels and buttons

--- HW2CIS357/ViewController.swift
+++ HW2CIS357/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var p1LatInputField: UITextField!
+    @IBOutlet weak var p2LatInputField: UITextField!
+    @IBOutlet weak var p1LongInputField: UITextField!
+    @IBOutlet weak var p2LongInputField: UITextField!
+    
+    var p1Lat: String = ""
+    var p2Lat: String = ""
+    var p1Long: String = ""
+    var p2Long: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        p1LatInputField.text = p1Lat
+        p2LatInputField.text = p2Lat
+        p1LongInputField.text = p1Long
+        p2LongInputField.text = p2Long
     }
 
 
+    @IBAction func calculate(_ sender: UIButton) {
+        
+        //Insert math to calculate distance and bearing
+    }
+    
+    
+    @IBAction func goToSettings(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    
 }
 
